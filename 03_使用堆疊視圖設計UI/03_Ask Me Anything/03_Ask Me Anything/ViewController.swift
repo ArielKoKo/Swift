52e540b9a5abdbeece6f0515efe292ea4c1e4c08
ArielKo <arielko9252@gmail.com>
2021-02-19 16:12:25 +0800

    03 & 04

---
+++ 03_使用堆疊視圖設計UI/03_Ask Me Anything/03_Ask Me Anything/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  S6 Homework #2
+//
+//  Created by Ariel Ko on 2021/1/21.
+//  Copyright © 2021 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 04_建立UITableView表格視圖/04_SimpleTable-2/04_SimpleTable-2/RestaurantTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantTableViewCell.swift
+//  S9 Homework #2
+//
+//  Created by Ariel Ko on 2021/1/26.
+//  Copyright © 2021 ArielKo. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
